23b559158c2b22540db7e9732f43f4da0ce128de
Will Dickson <wbd@caltech.edu>
2008-12-16 20:51:42 -0800

    Started adding structures to yawff.py

--- python/libyawff/yawff.py
+++ python/libyawff/yawff.py
@@ -29,3 +29,58 @@ License along with yawff.  If not, see
 import ctypes 
 import scipy
 
+lib = ctypes.cdll.LoadLibrary("libyawff.so.1")
+
+# Constants 
+MAX_MOTOR = lib.get_max_motor()
+MAX_DT = lib.get_max_dt()
+MIN_DT = lib.get_min_dt()
+CLOCK_HI_NS = lib.get_clock_hi_ns()
+
+# Structures
+class array_t(ctypes.Structure):
+    _fields_ = [
+        ('data', ctypes.c_void_p), 
+        ('nrow', ctypes.c_int), 
+        ('ncol', ctypes.c_int),
+        ('s0', ctypes.c_int),
+        ('s1', ctypes.c_int),
+        ('type', ctypes.c_int), 
+        ]
+
+class config_t(ctypes.Structure):
+    _fields_ = [
+        ('dev_name', ctypes.c_char_p),
+        ('ain_subdev', ctypes.c_uint),
+        ('dio_sudev', ctypes.c_int),
+        ('num_motor', ctypes.c_int),
+        ('yaw_motor', ctypes.c_int),
+        ('dio_clk', MAX_MOTOR*ctypes.c_int),
+        ('dio_dir', MAX_MOTOR*ctypes.c_int),
+        ('kine_map', MAX_MOTOR*ctypes.c_int),
+        ('dio_disable', ctypes.c_int),
+        ('yaw_ain', ctypes.c_uint),
+        ('yaw_volt2torq',ctypes.c_float),
+        ('yaw_inertia', ctypes.c_float),
+        ('yaw_ind2deg', ctypes.c_float),
+        ('yaw_torq_lim', ctypes.c_float),
+        ('yaw_filt_cut', ctypes.c_float),
+        ('yaw_damping', ctypes.c_float),
+        ('dt', ctypes.c_int),
+        ]
+
+class data_t(ctypes.Structure):
+    _fields_ = [
+        ('t', array_t),
+        ('pos', array_t),
+        ('vel', array_t),
+        ('torq', array_t),
+        ]
+
+# // Structure for return data
+# typedef struct {
+#   array_t t;
+#   array_t pos;
+#   array_t vel;
+#   array_t torq;
+# } data_t;
